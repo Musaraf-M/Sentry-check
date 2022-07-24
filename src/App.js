4735e1843e0502809437646d0bbeaccafdd8e8ff
Musaraf-M <mohammedmusaraf@psnacet.edu.in>
2022-07-24 20:06:34 +0530

    feat(web): configured sentry workspace

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const methodDoesNotExist = () => {
+    throw Error("Sentry log error");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={methodDoesNotExist}>Break the world</button>;
     </div>
   );
 }
